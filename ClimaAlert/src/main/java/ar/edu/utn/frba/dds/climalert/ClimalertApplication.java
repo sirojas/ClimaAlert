64a03dca7a0d4a016531d4f8661019656f01e20e
sirojas <sirojas@frba.utn.edu.ar>
2026-06-23 16:09:19 -0300

    creamos el repo

---
+++ ClimaAlert/src/main/java/ar/edu/utn/frba/dds/climalert/ClimalertApplication.java
@@ -0,0 +1,15 @@
+package ar.edu.utn.frba.dds.climalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ClimalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimalertApplication.class, args);
+	}
+
+}
